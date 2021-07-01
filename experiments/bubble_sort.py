a1b50e90d09d71b37a973fa48655367dbafc585b
argandov <armandogv25@gmail.com>
2021-07-01 16:30:57 -0500

    update bubble sort

--- experiments/bubble_sort.py
+++ experiments/bubble_sort.py
@@ -2,7 +2,9 @@
 
 # Bubble sort algorithm from Mashrur class
 
-# Entonces, O(n^2) A qué es igual?
+# O(n^2) Inconsistent?
+
+from random import randint
 
 def bubble_sort(l1):
     swap_state = 0 
@@ -14,12 +16,10 @@ def bubble_sort(l1):
     if swap_state == 0: # return 1 if the loops didn't change swap_state var
         return 1
 
- 
-l1 = [2, 1, 4, 3, 2]
-l2 = [34, 1, 2, 4, 3, 3, 5, 8, 87, 62, 68, 6, 52, 356, 78,\
-        876, 2, 45, 52, 2345, 6, 2, 1, 2, 7, 2, 1, 234, 7, 21, 23, 45]
+random_list = [randint(1,100) for num in range(1, 10)] # Random list of 10 numbers from 1-100
 sort_state = 0
-to_sort = l2    # To quickly change input lists
+to_sort = random_list    # To quickly change input lists
+
 print(f"[0] Sorting: {to_sort}")
 while not sort_state:
     if bubble_sort(to_sort):
