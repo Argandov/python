508342c75eeb2a6467f8e0ea276f8ccbc3dbb7c2
argandov <armandogv25@gmail.com>
2021-07-02 00:36:11 -0500

    update python exercises

--- exercises_lessons/generators_memory_test.py
+++ exercises_lessons/generators_memory_test.py
@@ -8,49 +8,49 @@ for memory usage
 import memory_profiler as mem_profile
 import random
 
-names = ['John', 'Corey', 'Adam', 'Steve', 'Rick', 'Thomas']
-majors = ['Math', 'Engineering', 'CompSci', 'Arts', 'Business']
+names = ['name1', 'name2', 'name3', 'name3', 'name4', 'name5']
+attributes = ['Math', 'Engineering', 'CompSci', 'Arts', 'Business']
 
 Mem1 = mem_profile.memory_usage()
 
 print(f"{Mem1} MB") # [38.640625] MB
 
 # Case 1: Generating a dictionary of 1000000 entries
-def people_list(num_people):
+def my_list(num):
     result = []
-    for i in range(num_people):
-        person = {
+    for i in range(num):
+        my_dict = {
                     'id': i,
                     'name': random.choice(names),
-                    'major': random.choice(majors)
+                    'attribute': random.choice(attributes)
                 }
-        result.append(person)
+        result.append(my_dict)
     return result
 
 # Case 2: Generator of 1000000 entries (Exactly the same data)
-def people_gen(num_people):
-    for i in range(num_people):
-        person = {
+def my_generator(num):
+    for i in range(num):
+        my_dict = {
                     'id': i,
                     'name': random.choice(names),
-                    'major': random.choice(majors)
+                    'attribute': random.choice(attributes)
                 }
-        yield person
+        yield my_dict
 
-# Use people_list OR people_gen to profile the performance and see the difference
-# people = people_list(1000000)       # ~ [310.24609375] MB
-people = people_gen(1000000)      # [38.640625] MB
+# Use my_list OR my_generator to profile the performance and see the difference
+test1 = my_list(1000000)       # ~ [310.24609375] MB
+# test1 = my_generator(1000000)      # [38.640625] MB
 
-# For people_gen, the memory usage DOES NOT change, even when unpacking the generator object:
+# For my_generator, the memory usage DOES NOT change, even when unpacking the generator object:
 # (Commented because it takes a lot of time to execute)
 # counter = 0
-# for i in people:
+# for i in test1:
 #     for k, v in i.items():
-#        if v == 'Thomas':
+#        if v == 'name4':
 #            counter += 1
 # print(counter)
 
-# Profiling the memory at the end of the program ("people" dicts in buffer)
+# Profiling the memory at the end of the program ("test1" dicts in buffer)
 Mem2 = mem_profile.memory_usage()
 print(f"{Mem2} MB")
 
